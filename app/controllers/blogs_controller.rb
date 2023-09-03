765e8f82dd5f2bb8693589285c804d0f81b5115b
natsuto6 <79001972+natsuto6@users.noreply.github.com>
2023-09-04 00:21:36 +0900

    他人のブログを編集・更新・削除できないよう制限

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,6 +4,7 @@ class BlogsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
 
   before_action :set_blog, only: %i[show edit update destroy]
+  before_action :authenticate_owner!, only: %i[edit update destroy]
 
   def index
     @blogs = Blog.search(params[:term]).published.default_order
@@ -49,5 +50,9 @@ class BlogsController < ApplicationController
 
   def blog_params
     params.require(:blog).permit(:title, :content, :secret, :random_eyecatch)
+  end
+
+  def authenticate_owner!
+    raise ActiveRecord::RecordNotFound unless @blog.user == current_user
   end
 end
